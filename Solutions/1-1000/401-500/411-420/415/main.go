8867af38448beb96af7c46834b4a1951ddc14be7
mats9693 <982254249@qq.com>
2020-08-03 16:17:39 +0800

    415

--- Solutions/1-1000/401-500/411-420/415/main.go
+++ Solutions/1-1000/401-500/411-420/415/main.go
@@ -7,35 +7,37 @@ func addStrings(num1 string, num2 string) string {
 		return num1
 	}
 
-	// make sure num1 is more short
-	if len(num1) > len(num2) {
+	if len(num1) > len(num2) { // make sure num1 is more short
 		num1, num2 = num2, num1
 	}
 
 	var (
-		i, j, k = len(num1) - 1, len(num2) - 1, len(num2) - 1
-		t, flag int
-		result  = make([]byte, len(num2), len(num2))
+		i, j, k        = len(num1) - 1, len(num2) - 1, len(num2) - 1 // k is index for 'arb'
+		addResultBytes = make([]byte, len(num2), len(num2))
+		addCarryFlag   uint8
 	)
 	for ; i >= 0; i, j, k = i-1, j-1, k-1 {
-		t = int(num1[i]-'0') + int(num2[j]-'0') + flag
+		t := (num1[i] - '0') + (num2[j] - '0') + addCarryFlag
 
-		result[k] = byte(t%10 + '0')
-		flag = t / 10
+		addCarryFlag = t / 10
+		addResultBytes[k] = t%10 + '0'
 	}
 
-	for ; j >= 0 && flag == 1; j, k = j-1, k-1 {
-		t = int(num2[j]-'0') + flag
+	for ; j >= 0 && addCarryFlag == 1; j, k = j-1, k-1 {
+		t := (num2[j] - '0') + addCarryFlag
 
-		result[k] = byte(t%10 + '0')
-		flag = t / 10
+		addCarryFlag = t / 10
+		addResultBytes[k] = t%10 + '0'
 	}
 
-	if flag == 1 {
-		return "1" + string(result)
-	} else if j != -1 {
-		return num2[:j+1] + string(result[j+1:])
+	var result string
+	if addCarryFlag == 1 {
+		result = "1" + string(addResultBytes)
+	} else if j >= 0 {
+		result = num2[:j+1] + string(addResultBytes[j+1:])
+	} else {
+		result = string(addResultBytes)
 	}
 
-	return string(result)
+	return result
 }
