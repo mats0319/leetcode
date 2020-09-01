974314e66df3e62ea3e4e7e75fcf96deb02f2608
mats9693 <982254249@qq.com>
2020-09-01 13:52:03 +0800

    486

---
+++ solutions/1-1000/401-500/481-490/486/main_test.go
@@ -0,0 +1,23 @@
+package mario
+
+import "testing"
+
+var testCase = []struct {
+	In     []int
+	Expect bool
+}{
+	// test cases here
+	{[]int{1,5,2}, false},
+	{[]int{1,5,233,7}, true},
+	{[]int{0}, true},
+	{[]int{1,5,2,4,6}, true},
+}
+
+func TestPredictTheWinner(t *testing.T) {
+	tcs := testCase
+	for i := range tcs {
+		if PredictTheWinner(tcs[i].In) != tcs[i].Expect {
+			t.Errorf("predict the winner test failed on case: %d\n", i)
+		}
+	}
+}
