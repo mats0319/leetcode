a6a29c008957ccedb8a0f9c233665a088f7706ce
mats9693 <982254249@qq.com>
2020-10-24 17:01:10 +0800

    1024 memory improve

--- solutions/1001-2000/1001-1100/1021-1030/1024/main.go
+++ solutions/1001-2000/1001-1100/1021-1030/1024/main.go
@@ -1,39 +1,37 @@
 package mario
 
 func videoStitching(clips [][]int, T int) int {
-    end := 0
-    count := 0
-    for end < T && end != -1 {
-        clips, end = nextInterval(clips, end)
-        count++
-    }
+	count := 0
+	for end, index := 0, len(clips)-1; end < T; count++ {
+		index, end = nextInterval(clips, index, end)
 
-    if end == -1 {
-        count = -1
-    }
+		if end == -1 {
+			count = -1
+			break
+		}
+	}
 
-    return count
+	return count
 }
 
-func nextInterval(src [][]int, limit int) ([][]int, int) {
-    index := len(src)-1
-    end := limit
-    for i := 0; i <= index; i++ {
-        if src[i][0] <= limit {
-            if src[i][1] > end {
-                end = src[i][1]
-            }
+func nextInterval(src [][]int, index, limit int) (int, int) {
+	end := limit
+	for i := 0; i <= index; i++ {
+		if src[i][0] <= limit {
+			if src[i][1] > end {
+				end = src[i][1]
+			}
 
-            src[i], src[index] = src[index], src[i]
-            index--
-            i--
-        }
-    }
+			src[i], src[index] = src[index], src[i]
+			index--
+			i--
+		}
+	}
 
-    // no new longer interval or src is nil
-    if end == limit {
-        end = -1
-    }
+	// no new longer interval or src is nil
+	if end == limit {
+		end = -1
+	}
 
-    return src[:index+1], end
+	return index, end
 }
