b85863e98728da7a9742ff46ba69e1a91346f811
mats9693 <982254249@qq.com>
2020-10-25 21:35:01 +0800

    845

---
+++ solutions/1-1000/801-900/841-850/845/main_test.go
@@ -0,0 +1,21 @@
+package mario
+
+import "testing"
+
+var testCase = []struct {
+	In     []int
+	Expect int
+}{
+	// test cases here
+	{[]int{2,1,4,7,3,2,5}, 5},
+	{[]int{1,4,7,3,2}, 5},
+}
+
+func TestLongestMountain(t *testing.T) {
+	tcs := testCase
+	for i := range tcs {
+		if longestMountain(tcs[i].In) != tcs[i].Expect {
+			t.Errorf("longest mountain test failed on case: %d\n", i)
+		}
+	}
+}
