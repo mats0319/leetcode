88347c8b8d1a28a40636ef71b4e70d5a9c04d6d3
mats9693 <982254249@qq.com>
2020-10-27 08:18:46 +0800

    144

---
+++ solutions/1-1000/101-200/141-150/144/main.go
@@ -0,0 +1,25 @@
+package mario
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+    return recurseTraversal(root, make([]int, 0))
+}
+
+func recurseTraversal(root *TreeNode, res []int) []int {
+    if root == nil {
+        return res
+    }
+
+    res = append(res, root.Val)
+
+    res = recurseTraversal(root.Left, res)
+
+    res = recurseTraversal(root.Right, res)
+
+    return res
+}
