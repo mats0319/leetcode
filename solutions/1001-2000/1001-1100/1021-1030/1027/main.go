c386117a09ebe4b947b557820114c0470728b326
mats9693 <982254249@qq.com>
2020-10-28 08:16:54 +0800

    1027

---
+++ solutions/1001-2000/1001-1100/1021-1030/1027/main.go
@@ -0,0 +1,22 @@
+package mario
+
+func uniqueOccurrences(arr []int) bool {
+    frequency := make(map[int]int) // elem: appear times
+    for _, v := range arr {
+        frequency[v]++
+    }
+
+    isUnique := true
+    elem := make(map[int]int) // appear times: last elem
+    for key, value := range frequency {
+        if _, ok := elem[value]; ok {
+            isUnique = false
+            break
+        }
+
+        elem[value] = key
+    }
+
+    return isUnique
+}
+
